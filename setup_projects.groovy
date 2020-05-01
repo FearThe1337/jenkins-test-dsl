a50d0c3dce8054d7d605c03df5d54521e1a5c885
Dummy <Dummy@example.com>
2020-04-30 21:26:11 -0600

    oops

--- setup_projects.groovy
+++ setup_projects.groovy
@@ -1,9 +1,11 @@
 job('001-setup-jenkins-job') {
     steps {
-        additionalClasspath('jobs')
-        removeAction('DISABLE')
-        removeViewAction('DELETE')
-        lookupStrategy('JENKINS_ROOT')
-        external('_000000_sample_job.groovy')
+        dsl {
+            additionalClasspath('jobs')
+            removeAction('DISABLE')
+            removeViewAction('DELETE')
+            lookupStrategy('JENKINS_ROOT')
+            external('_000000_sample_job.groovy')
+        }
     }
 }
