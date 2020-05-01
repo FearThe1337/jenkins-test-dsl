2e5a9b2e4b981ef9e8e0eeeeb39f152d872220b2
Dummy <Dummy@example.com>
2020-04-30 20:44:32 -0600

    Add sample job

---
+++ jobs/000000-sample-job.groovy
@@ -0,0 +1,19 @@
+multibranchPipelineJob('multibranch-test') {
+    branchSources {
+        git {
+            // id has to be unique, so random uuid?
+            id('481be7f2-933a-4456-a9ca-b5ba4d147eef')
+            remote('https://github.com/jenkinsci/job-dsl-plugin.git')
+            // credentialsId('id')
+            includes('*')
+        }
+    }
+    orphanedItemStrategy {
+        discardOldItems {
+            numToKeep(20)
+        }
+    }
+
+    description('My multibranch pipeline test')
+    displayName('multibranch-test')
+}
