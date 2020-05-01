82c7d1c139dbe819689cb2a278e98e9643480ce9
Dummy <Dummy@example.com>
2020-04-30 21:44:27 -0600

    Looks like classpath does not work for external.

--- setup_projects.groovy
+++ setup_projects.groovy
@@ -13,11 +13,10 @@ job('create_jobs') {
     }
     steps {
         dsl {
-            additionalClasspath('jobs')
             removeAction('DISABLE')
             removeViewAction('DELETE')
             lookupStrategy('JENKINS_ROOT')
-            external('_000000_sample_job.groovy')
+            external('jobs/_000000_sample_job.groovy')
         }
     }
 }
